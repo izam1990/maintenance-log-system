15d984a1004a85663acc6f3d52b612313c73a9ba
izam1990 <izam1990@gmail.com>
2026-05-01 00:33:48 +0300

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -29,6 +29,16 @@ security = HTTPBearer()
 
 app = FastAPI()
 api_router = APIRouter(prefix="/api")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://maintenance-frontend-gn0w.onrender.com",
+        "http://localhost:3000"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class User(BaseModel):
